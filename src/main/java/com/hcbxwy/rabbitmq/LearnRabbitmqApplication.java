14cfe82577038e2f4927b5adb440fedc1a91e1e4
Billson <Billson@hcbxwy.com>
2021-03-28 15:02:11 +0800

    初始化

---
+++ src/main/java/com/hcbxwy/rabbitmq/LearnRabbitmqApplication.java
@@ -0,0 +1,19 @@
+package com.hcbxwy.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 应用启动入口
+ *
+ * @author Billson
+ * @date 2021/3/28 14:51
+ */
+@SpringBootApplication
+public class LearnRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnRabbitmqApplication.class, args);
+    }
+
+}
