c28f3b637087e4f0eb77b36438c2359e22f6a312
Billson <billson@hcbxwy.com>
2021-07-10 11:47:00 +0800

    优化代码

--- learn-rabbitmq-producer/src/main/java/com/hcbxwy/rabbitmq/controller/SendMessageController.java
+++ learn-rabbitmq-producer/src/main/java/com/hcbxwy/rabbitmq/controller/SendMessageController.java
@@ -25,14 +25,14 @@ public class SendMessageController {
      * 发送消息到主题交换机
      *
      * @param msg 消息内容
-     * @param key 路由key
+     * @param routingKey 路由key
      * @return java.lang.String
      * @author Billson
      * @date 2021/3/31 下午8:41
      */
     @GetMapping("/send/topic")
-    public String sendTopic(@RequestParam String msg, @RequestParam String key) {
-        rabbitTemplate.convertAndSend(DefaultExchange.TOPIC, key, msg);
+    public String sendTopic(@RequestParam String msg, @RequestParam String routingKey) {
+        rabbitTemplate.convertAndSend(DefaultExchange.TOPIC, routingKey, msg);
         return "发送消息成功！";
     }
 
@@ -54,14 +54,14 @@ public class SendMessageController {
      * 发送消息到直连交换机
      *
      * @param msg 消息内容
-     * @param key 路由key
+     * @param routingKey 路由key
      * @return java.lang.String
      * @author Billson
      * @date 2021/3/31 下午8:41
      */
     @GetMapping("/send/direct")
-    public String sendDirect(@RequestParam String msg, @RequestParam String key) {
-        rabbitTemplate.convertAndSend(DefaultExchange.DIRECT, key, msg);
+    public String sendDirect(@RequestParam String msg, @RequestParam String routingKey) {
+        rabbitTemplate.convertAndSend(DefaultExchange.DIRECT, routingKey, msg);
         return "发送消息成功！";
     }
 }
